3a3df243a27f05cd69f9ec064b91c3d53fae4b1a
nee2Singh <110149256+nee2Singh@users.noreply.github.com>
2022-07-29 13:50:23 +0530

    Add files via upload

---
+++ AlphabetsPattern.java
@@ -0,0 +1,309 @@
+package com.assignment.ineuron;
+
+public class AlphabetsPattern {
+
+	public static void main(String[] args) 
+	{
+		int n = 9;
+		
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				if((i > 0 && (j == 0 || j == n-1)) || i == (n-1)/2 || (i == 0 & j > 0 && j < n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || (i == 0 && j < n-1) || (i == (n-1)/2 && j < n-1) || (i == n-1 && j < n-1) || (i > 0 && i < (n-1)/2 && j == n-1)
+						|| (i > (n-1)/2 && i < n-1 && j == n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((i > 0 && i < n-1 && j == 0) || (i == 0 && j > 0 && j < n-1) || (i == n-1 && j > 0 && j < n-1) || ((i == 1 || i == n-2) && j == n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((j == n-1 && i > 0 && i < n-1) || j == 0 || (i == 0 && j < n-1) || (i == n-1 && j< n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || j == 0 || i == n-1 || i == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || j == 0 || i == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if( j== 0 || (i == 0 && j < n-1) || (i == n-1 && j <= (n-1)/2) || (i >= (n-1)/2 && (j == (n-1)/2 || j == n-1) 
+						|| ((i == 1 || i == n-1) && j == n-1)) || (j >= (n-1)/2 && i == (n-1)/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || j == n-1 || i == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || i == n-1 || j == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || j == (n-1)/2 || (j > 0 && j < (n-1)/2 && i == n-1) || (j == 0 && i == n-2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || i+j == (n-1)/2 || i-j == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || i == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || i == j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i+j == n-1 || j == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || j == n-1 || i == j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((i > 0 && i < n-1 && (j == 0 || j == n-1)) || ((i == 0 || i == n-1) && j > 0 && j < n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || (i == 0 && j < n-1) || (i == (n-1)/2 && j < n-1) || (j == n-1 && i > 0 && i < (n-1)/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((i > 0 && i < n-3 && (j == 0 || j == n-1)) || (j > 0 && j < n-1 && (i == 0 || i == n-3)) || (j >= (n-1)/2 && i >= (n-1)/2 && i == j))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || ((i == 0 || i == (n-1)/2) && (j > 0 && j < n-1)) || (j == n-1 && i > 0 && i < (n-1)/2) || i-j == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((i > 0 && i < (n-1)/2 && j == 0) || (j > 0 && j < n-1 && (i == 0 || i == (n-1)/2 || i == n-1)) || (i > (n-1)/2 && i < n-1 && j == n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || j == (n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(((j == 0 || j == n-1) && i >= 0 && i < n-1) || (i == n-1 && j > 0 && j < n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i==j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			for(int j = 0; j < n; j++)
+			{
+				if(i+j == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				if(j == 0 || i+j == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			for(int j = 0; j < n; j++)
+			{
+				if(j == n-1 || i==j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i==j || i+j == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if((i==j && i <= (n-1)/2 && j <= (n-1)/2)  || (i+j == n-1 && i <= (n-1)/2 && j >= (n-1)/2) || (i >= (n-1)/2 && j == (n-1)/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.print("	");
+			
+			for(int j = 0; j < n; j++)
+			{
+				if(i == 0 || i == n-1 || i+j == n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.println();
+		}
+
+		
+	}
+
+}
